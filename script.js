87a3eeb1a508ec18cbcb7e84dd1a4b9d4310751b
Fnatik11 <91971731+Fnatik11@users.noreply.github.com>
2022-02-03 17:55:11 +0300

    homework les33

---
+++ script.js
@@ -0,0 +1,14 @@
+// let num1 = 60
+// let num2 = 1
+// while (num2 < 11){
+//     console.log(num1 * num2)
+//     num2 ++
+// }
+
+// let a = 1
+// let b = 9
+// while(a < 10 , b > 0){
+//     console.log(a + b)
+//     a ++
+//     b --
+// }
